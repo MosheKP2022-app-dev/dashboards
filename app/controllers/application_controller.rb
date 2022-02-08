427b9c87ea8dd79c90cc30a3fdf7243f18a47e13
MosheKP2022 <mperry7@chicagobooth.edu>
2022-02-08 03:26:31 +0000

    forex page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+
+  def currency_pairs
+
+    @currency_url = "https://api.exchangerate.host/symbols"
+      
+    @raw_json_string = open(@currency_url).read
+
+    @parsed_json = JSON.parse(@raw_json_string)
+
+    @symbols = @parsed_json.fetch("symbols")
+
+    @currency_hash = @symbols.keys
+
+    @currency_hash.each do |currency|
+
+    
+
+    end
+
+    @currency = @currency_hash.at(0)
+
+
+
+    render({ :template=> "currency_templates/convert_first_currency.html.erb"})
+ 
+
+  end
 end
