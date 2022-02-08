1512c8c8140af6f92a6b65dc558fa3b0f9a489d8
MosheKP2022 <mperry7@chicagobooth.edu>
2022-02-08 04:01:14 +0000

    forex page+ first currency page works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  def currency_pairs
+  def first_currency
 
     @currency_url = "https://api.exchangerate.host/symbols"
       
@@ -15,4 +15,21 @@ class ApplicationController < ActionController::Base
     render({ :template=> "currency_templates/convert_first_currency.html.erb"})
  
   end
+
+  def second_currency
+
+    @currency_url = "https://api.exchangerate.host/symbols"
+      
+    @raw_json_string = open(@currency_url).read
+
+    @parsed_json = JSON.parse(@raw_json_string)
+
+    @symbols = @parsed_json.fetch("symbols")
+
+    @currency_hash = @symbols.keys
+
+    @first_currency = params.fetch("currency")
+
+    render({ :template=> "currency_templates/convert_second_currency.html.erb"})
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get("/forex", { :controller => "application", :action => "currency_pairs" })
+  get("/forex", { :controller => "application", :action => "first_currency" })
+  get("/forex/:currency", { :controller => "application", :action => "second_currency" })
   #get("/forex/:first_currency/:second_currency", { :controller => "application", :action => "currency_pairs" })
   #get("/forex/:first_currency/:second_currency", { :controller => "application", :action => "currency_pairs" })
 
